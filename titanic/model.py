aa9d02d7294b529beb3b6c05d09227ae16398386
MinhyeSim <smine0032@gmail.com>
2022-03-15 17:21:52 +0900

    17:21 titanic 알고리즘 데이터 유형 4가지 구분하기

--- titanic/model.py
+++ titanic/model.py
@@ -14,3 +14,45 @@ class TitanicModel(object):
         ic(f'트레인 컬럼 {self.train.columns}')
         ic(f'트레인 헤드 {self.train.head()}')
         ic(self.train)
+
+    def preprocess(self):
+        self.create_label()
+
+    def create_label(self)->object:
+        pass
+
+    def create_train(self)->object:
+        pass
+
+    def drop_feature(self)->object:
+        pass
+
+    def pclass_ordinal(self)->object:
+        pass
+
+    def name_nominal(self)->object: #garbage
+        pass
+
+    def sex_nominal(self)->object:
+        pass
+
+    def age_ordinal(self)->object:
+        pass
+
+    def sibsp_nominal(self)->object:
+        pass
+
+    def parch_nominal(self)->object:
+        pass
+
+    def ticket_nominal(self)->object: #garbage
+        pass
+
+    def fare_ratio(self)->object: #garbage
+        pass
+
+    def cabin_nominal(self)->object:
+        pass
+
+    def embarked_nominal(self)->object: #garbage
+        pass
